bd090d9989c9d72fc72bc0e67871b58b26e0a94c
David <ihimdavid@gmail.com>
2020-07-14 15:14:04 +0000

    Add correct time range if no attendees in meeting request or duration of meeting is longer than the day's duration

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -14,10 +14,32 @@
 
 package com.google.sps;
 
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.HashSet;
 
 public final class FindMeetingQuery {
+
+  
+  /**
+   * Given meeting information and all events on a certain day
+   * Returns the times when the meeting can happen
+   */ 
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
+    // No attendees return whole day time interval
+    if (request.getAttendees().isEmpty()) return Arrays.asList(TimeRange.WHOLE_DAY);
+
+    // Duration longer than a day return empty collection
+    if (request.getDuration() > TimeRange.WHOLE_DAY.duration()) return Arrays.asList();
+
+    /* Meeting: name, duration in minutes, collection of attendees */
+    /* Event: name, time range, collection of attendees */
+    /* time range: start time, end time, duration */
+    // If attendees of a event not apart of the meeting it shouldn't effect meeting
+    // One event split time zones into two options
+    // Consider different events of attendees
+    // Events that are subsets of another
+    // No conflicts & all conflicts
+    return Arrays.asList();
   }
 }
